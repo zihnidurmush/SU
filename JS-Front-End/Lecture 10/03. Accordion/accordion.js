6a16c8538000d67132206f62a9caaa71f266632e
Zihni Durmush <zdurmush@gmail.com>
2024-08-02 20:46:18 +0300

    commit

--- JS-Front-End/Lecture 10/03. Accordion/accordion.js
+++ JS-Front-End/Lecture 10/03. Accordion/accordion.js
@@ -1,9 +1,13 @@
 function toggle() {
-    const toggleButtonElement = document.querySelector('.head span.button');
     const extraInformationELement = document.getElementById('extra');
-
+    const toggleButtonElement = document.querySelector('.head span.button');
     
-
-
-
+    const currentButtonText = toggleButtonElement.textContent;
+    if (currentButtonText === 'More') {
+        extraInformationELement.style.display = 'block';
+        toggleButtonElement.textContent = 'Less';
+    } else {
+        extraInformationELement.style.display = 'none';
+        toggleButtonElement.textContent = 'More';
+    }
 }
